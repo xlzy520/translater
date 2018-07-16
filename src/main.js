6bea1e9b29a8fb6965b156780b5275fd01a54c59
zhibi <1017866168@qq.com>
2018-07-16 16:35:32 +0800

    不使用Flexible伸缩布局

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-import 'amfe-flexible'
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 // By default we import all the components.
 // Only reserve the components on demand and remove the rest.
